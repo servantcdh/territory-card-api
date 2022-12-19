46352687babb56c846e3401bb14953bbea02f254
servantcdh <servantcdh@gmail.com>
2022-12-19 19:50:42 +0900

    Refactor: "Change app.module to import environment variables"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Members } from './members/entities/members.entity';
 import { Accesses } from './auth/entities/accesses.entity';
@@ -26,7 +27,17 @@ import { TerritoryRecordContents } from './records/entities/territory-record-con
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.development.env'],
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      })
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
@@ -51,6 +62,7 @@ import { TerritoryRecordContents } from './records/entities/territory-record-con
         TerritoryRecords,
         TerritoryRecordContents
       ],
+      logging: true,
       synchronize: true, // Be careful ㅜㅜㅜ 그치만 신기해
     }),
     AuthModule,
