653853a5ca949b484e5bd827385e3e1b97e0b868
servantcdh <servantcdh@gmail.com>
2022-12-19 18:56:47 +0900

    Feat: "Add ValidationPipe with options, whitelist, forbidNonWhitelisted, transform"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에 명시되지 않은 파라미터는 받지 않음
+      forbidNonWhitelisted: true, // dto에 명시되지 않은 파라미터로 요청이 오면 에러 반환
+      transform: true, // 파라미터의 string 값을 실제 타입으로 변환
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
