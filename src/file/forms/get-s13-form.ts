b507449679d79b7ea8bfa5a263aac499091a82a2
servantcdh <servantcdh@gmail.com>
2022-12-29 00:39:44 +0900

    Comment: "Add comment TODO s-13 template"

--- src/file/forms/get-s13-form.ts
+++ src/file/forms/get-s13-form.ts
@@ -1,46 +1,56 @@
 import * as fs from 'fs';
 import createReport from 'docx-templates';
-import { TerritoryRecord } from 'src/record/entities/territory-record.entity';
 import { StreamableFile } from '@nestjs/common';
 
-export const getS13 = async (territoryRecord: TerritoryRecord[]) => {
+export const getS13 = async (territoryRecord: any[]) => {
   try {
-    // TODO: 다섯 번째 카드 기록 부터는 다음 페이지로 넘어가도록 구현
+    // TODO: 템플릿 반복문 수정 필요
     const template = fs.readFileSync('static/S-13_KO_template.docx');
 
-    const records = territoryRecord.map((record) => {
-      const contents = [];
-      record.territoryRecordContent.forEach((c, i) => {
-        if (i < 4) {
-          contents.push(c);
+    const docx: FormS13 = {
+      serviceYear: territoryRecord[0].serviceYear,
+      rows: [],
+    };
+
+    territoryRecord.forEach((record, rowIdx) => {
+      const records = record.territoryRecordContent;
+      records.forEach((trContent, contentIdx) => {
+        const { user, dateAssigned, dateCompleted } = trContent;
+        const { name: nameAssignedTo } = user;
+        const content: ContentS13 = {
+          nameAssignedTo,
+          dateAssigned,
+          dateCompleted,
+        };
+        if (contentIdx % 4 === 0) {
+          docx.rows.push({
+            cardIdx: record.card.idx,
+            cardName: record.card.name,
+            lastDateCompleted: record.lastDateCompleted,
+            contents: [content],
+          });
+        } else {
+          const contents = docx.rows[docx.rows.length - 1].contents;
+          contents.push(content);
         }
       });
+      const contents = docx.rows[docx.rows.length - 1].contents;
       for (let i = 0; i < 4; i++) {
         if (!contents[i]) {
           contents.push({
+            nameAssignedTo: '',
             dateAssigned: '',
             dateCompleted: '',
-            user: {
-              name: '',
-            },
           });
         }
       }
-      return {
-        ...record,
-        territoryRecordContent: contents,
-      };
     });
-    const data = {
-      serviceYear: territoryRecord[0].serviceYear,
-      territoryRecord: records,
-    };
+
+    console.log(docx.rows);
 
     const s13 = await createReport({
       template,
-      data: {
-        data,
-      },
+      data: { docx },
     });
 
     return new StreamableFile(s13);
@@ -49,3 +59,21 @@ export const getS13 = async (territoryRecord: TerritoryRecord[]) => {
     // throw new NotFoundException('s-13 워드 템플릿 확인 필요');
   }
 };
+
+export interface FormS13 {
+  serviceYear: number;
+  rows: RowS13[];
+}
+
+export interface RowS13 {
+  cardIdx: number;
+  cardName: string;
+  lastDateCompleted: string;
+  contents: ContentS13[];
+}
+
+export interface ContentS13 {
+  nameAssignedTo: string;
+  dateAssigned: string;
+  dateCompleted: string;
+}
