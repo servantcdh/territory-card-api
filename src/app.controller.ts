db9bebae7237a6639e98f55b7c6c3481fe66029d
DonghoChoi <servantcdh@naver.com>
2023-01-18 02:32:07 +0900

    Feat: "for ELB HealthCheck"

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   home(): string {
     return this.appService.getHello();
   }
-
 }
