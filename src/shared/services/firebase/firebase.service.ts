31bd174db02a9f8ba4abf78e4e55e0b88a5c1448
DonghoChoi <servantcdh@naver.com>
2023-01-18 22:44:57 +0900

    Feat: "Add firebaseService sendAll"

--- src/shared/services/firebase/firebase.service.ts
+++ src/shared/services/firebase/firebase.service.ts
@@ -10,9 +10,23 @@ export class FirebaseService {
   constructor() {}
 
   sendPush(pushTokens: string[], title: string, body: string) {
-    return admin.messaging().sendToDevice(pushTokens, {
-      notification: { title, body, icon: './android-chrome-512x512.png', },
-      data: { title, body, icon: './android-chrome-512x512.png' },
-    });
+    const push = {
+      data: { icon: './android-chrome-512x512.png' },
+      token: '',
+      notification: {
+        title,
+        body,
+      },
+      webpush: {
+        fcmOptions: {
+          link: 'https://www.jwterritory.co.kr',
+        },
+      },
+    };
+    return admin.messaging().sendAll(pushTokens.map((token) => ({ ...push, token })));
+    // return admin.messaging().sendToDevice(pushTokens, {
+    //   notification: { title, body, icon: './android-chrome-512x512.png' },
+    //   data: { title, body, icon: './android-chrome-512x512.png' },
+    // });
   }
 }
